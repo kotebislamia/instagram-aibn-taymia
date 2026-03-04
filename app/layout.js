dc6a4dcc7e04164267587b1abd8e0c576de48f88
kotebislamia <flint200371@gmail.com>
2026-03-04 16:54:26 +0300

    Enhance metadata with icon and Open Graph details
    Added icon metadata and updated Open Graph images.

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,21 @@
 export const metadata = {
   title: 'Instagram',
   description: 'View this profile on Instagram',
+  icons: {
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png',
+  },
   openGraph: {
     title: 'Instagram',
     description: 'See photos and videos from this user',
-    images: ['https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png'],
+    url: 'https://instagram.com',
+    siteName: 'Instagram',
+    images: [
+      {
+        url: 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png',
+        width: 800,
+        height: 600,
+      },
+    ],
     type: 'website',
   },
 }
@@ -12,7 +23,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body style={{ margin: 0 }}>{children}</body>
     </html>
   )
 }
